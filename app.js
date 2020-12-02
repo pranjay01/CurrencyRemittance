ad3c8c2d3c137bf6d0a02e7f4bb1189fd5095396
Sourabh Garg <sourabhgarg7494>
2020-12-02 00:11:53 -0800

    app.js change

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
-app.get("/ping", (req, res) => {
-  res.json({
-    Test: "Privacy Policy"
-  });
-});
+// app.get("/ping", (req, res) => {
+//   res.json({
+//     Test: "Privacy Policy"
+//   });
+// });
 
 //Run application
 const port = process.env.PORT || 3000;
